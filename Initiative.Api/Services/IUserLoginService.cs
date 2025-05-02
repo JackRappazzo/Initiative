b985aa687ab878ab63761e03b5008de1ac709d5d
John Rappazzo <j360thunder@hotmail.com>
2025-05-02 14:01:16 -0400

    Create login service

---
+++ Initiative.Api/Services/IUserLoginService.cs
@@ -0,0 +1,8 @@
+﻿
+namespace Initiative.Api.Services
+{
+    public interface IUserLoginService
+    {
+        Task<(bool success, string message)> Login(string email, string password, CancellationToken cancellationToken);
+    }
+}
---
+++ Initiative.Api/Services/UserLoginService.cs
@@ -0,0 +1,42 @@
+﻿using Initiative.Api.Core.Identity;
+using Microsoft.AspNetCore.Identity;
+
+namespace Initiative.Api.Services
+{
+    public class UserLoginService : IUserLoginService
+    {
+        UserManager<InitiativeUser> userManager;
+
+        public UserLoginService(UserManager<InitiativeUser> userManager)
+        {
+            this.userManager = userManager;
+        }
+
+        public async Task<(bool success, string message)> Login(string email, string password, CancellationToken cancellationToken)
+        {
+            if (!string.IsNullOrEmpty(email) && !string.IsNullOrEmpty(password))
+            {
+                var user = await userManager.FindByNameAsync(email);
+
+                if (user == null)
+                {
+                    return (false, "No matching email address");
+                }
+
+                var passwordMatch = await userManager.CheckPasswordAsync(user, password);
+                if (passwordMatch)
+                {
+                    return (true, string.Empty);
+                }
+                else
+                {
+                    return (false, "Incorrect password");
+                }
+            }
+            else
+            {
+                return (false, "Missing arguments");
+            }
+        }
+    }
+}
