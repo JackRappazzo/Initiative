e6cc72eae722a8fc8793a56283719c74809585cb
John Rappazzo <j360thunder@hotmail.com>
2025-06-20 15:21:51 -0400

    Basic SignalR setup (partial)

---
+++ Initiative.Lobby.Core/LobbyHub.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace Initiative.Lobby.Core
+{
+    public class LobbyHub : Hub
+    {
+
+        public async Task SendNextTurn(string sender)
+        {
+            await Clients.All.SendAsync("NextTurn", sender);
+        }
+
+        public async Task SendCreatureList(string sender, List<string> creatureList)
+        {
+            await Clients.All.SendAsync("InitiativeList", sender, creatureList);
+        }
+
+        public async Task JoinLobby(string lobbyName)
+        {
+            await Groups.AddToGroupAsync(Context.ConnectionId, lobbyName);
+            await Clients.Group(lobbyName).SendAsync("UserJoined", Context.ConnectionId);
+        }
+
+        public async Task LeaveLobby(string lobbyName)
+        {
+            await Groups.RemoveFromGroupAsync(Context.ConnectionId, lobbyName);
+            await Clients.Group(lobbyName).SendAsync("UserLeft", Context.ConnectionId);
+        }
+
+    }
+}
---
+++ Initiative.Lobby.Core/Services/Lobby.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Initiative.Lobby.Core.Services
+{
+    public class Lobby
+    {
+        public string OwnerId { get; set; }
+        public string LobbyKey { get; set; }
+
+        public IEnumerable<string> ConnectionIds {  get=> connectionIds.ToImmutableList(); }
+
+        private List<string> connectionIds = new List<string>();
+
+        public Lobby(string ownerId, string lobbyKey)
+        {
+            OwnerId = ownerId;
+            LobbyKey = lobbyKey;
+        }
+
+        public void AddConnection(string connectionId)
+        {
+            if (!connectionIds.Contains(connectionId))
+            {
+                connectionIds.Add(connectionId);
+            }
+        }
+
+        public void RemoveConnection(string connectionId)
+        {
+            if (connectionIds.Contains(connectionId))
+            {
+                connectionIds.Remove(connectionId);
+            }
+        }
+
+    }
+}
