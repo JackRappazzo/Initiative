fab9e5b3e4072ed20ce6f2c79da4a154be68dc35
John Rappazzo <j360thunder@hotmail.com>
2025-07-21 20:06:33 -0400

    Remove AI generated "public" test methods

--- Initiative.Import.Core/Services/ICreatureMapper.cs
+++ Initiative.Import.Core/Services/ICreatureMapper.cs
@@ -8,19 +8,5 @@ namespace Initiative.Import.Core.Services
     {
         Creature MapToCreature(MonsterJson monsterJson);
         List<Creature> MapToCreatures(IEnumerable<MonsterJson> monsters);
-        
-        /// <summary>
-        /// Public method to extract condition immunities for testing purposes
-        /// </summary>
-        /// <param name="conditionImmunitiesList">List of JSON elements representing condition immunities</param>
-        /// <returns>List of condition immunity names</returns>
-        List<string> ExtractConditionImmunitiesPublic(List<JsonElement>? conditionImmunitiesList);
-        
-        /// <summary>
-        /// Public method to extract damage resistances for testing purposes
-        /// </summary>
-        /// <param name="damageResistancesList">List of JSON elements representing damage resistances</param>
-        /// <returns>List of damage resistance objects</returns>
-        List<DamageResistanceJson> ExtractDamageResistancesPublic(List<JsonElement>? damageResistancesList);
     }
 }
