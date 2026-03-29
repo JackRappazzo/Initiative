4974732e3cd63ec0f5ec0410310fb8ee9d1cfaee
John Rappazzo <j360thunder@hotmail.com>
2026-03-29 01:04:18 -0400

    Add logger

---
+++ Initiative.Logging/IStructuredLogger.cs
@@ -0,0 +1,23 @@
+﻿using Serilog.Events;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Initiative.Logging
+{
+    public interface IStructuredLogger
+    {
+
+        void Verbose(string messageTemplate, params object?[]? propertyValues);
+        void Debug(string messageTemplate, params object?[]? propertyValues);
+        void Information(string messageTemplate, params object?[]? propertyValues);
+        void Warning(Exception ex, string messageTemplate, params object?[]? propertyValues);
+        void Warning(string messageTemplate, params object?[]? propertyValues);
+        void Error(string messageTemplate, params object?[]? propertyValues);
+        void Error(Exception ex, string messageTemplate, params object?[]? propertyValues);
+
+        IStructuredLogger ForContext(string propertyName, object? value);
+    }
+}
---
+++ Initiative.Logging/StructuredLogger.cs
@@ -0,0 +1,53 @@
+﻿using Serilog;
+
+namespace Initiative.Logging
+{
+    public class StructuredLogger : IStructuredLogger
+    {
+        private readonly ILogger _logger;
+        public StructuredLogger(ILogger logger)
+        {
+            _logger = logger;
+        }
+
+        public void Debug(string messageTemplate, params object?[]? propertyValues)
+        {
+            _logger.Debug(messageTemplate, propertyValues);
+        }
+
+        public void Error(string messageTemplate, params object?[]? propertyValues)
+        {
+            _logger.Error(messageTemplate, propertyValues);
+        }
+
+        public void Error(Exception ex, string messageTemplate, params object?[]? propertyValues)
+        {
+            _logger.Error(ex, messageTemplate, propertyValues);
+        }
+
+        public void Information(string messageTemplate, params object?[]? propertyValues)
+        {
+            _logger.Information(messageTemplate, propertyValues);
+        }
+
+        public void Verbose(string messageTemplate, params object?[]? propertyValues)
+        {
+            _logger.Verbose(messageTemplate, propertyValues);
+        }
+
+        public void Warning(Exception ex, string messageTemplate, params object?[]? propertyValues)
+        {
+            _logger.Warning(ex, messageTemplate, propertyValues);
+        }
+
+        public void Warning(string messageTemplate, params object?[]? propertyValues)
+        {
+            _logger.Warning(messageTemplate, propertyValues);
+        }
+
+        public IStructuredLogger ForContext(string propertyName, object? value)
+        {
+            return new StructuredLogger(_logger.ForContext(propertyName, value));
+        }
+    }
+}
